61e409479ceae1cc9ac4919f349afffed09007ba
David Ostler <davidtostler@gmail.com>
2016-04-02 14:39:39 -0600

    Angularized the signup page.

--- client/app/scripts/controllers/signup.js
+++ client/app/scripts/controllers/signup.js
@@ -1,17 +1,50 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name clientApp.controller:SignupCtrl
- * @description
- * # SignupCtrl
- * Controller of the clientApp
- */
 angular.module('clientApp')
-  .controller('SignupCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('SignupCtrl', function () {
+
+		var user, signup;
+
+		signup = this;
+		user = signup.user = {};
+
+		// This is our method that will post to our server.
+		signup.submit = function () {
+			
+			// Make sure all fields are filled out...
+			if (
+				!user.firstname ||
+				!user.lastname ||
+				!user.email ||
+				!user.password1 ||
+				!user.password2
+			) {
+				window.alert('Please fill out all form fields.');
+				return false;
+			}
+
+			// make sure the passwords match match
+			if (user.password1 !== user.password2) {
+				window.alert('Your passwords must match.');
+				return false;
+			}
+
+			// Just so we can confirm that the bindings are working
+			console.log(user);
+
+			/*
+			// Make the request to the server
+			var request = $http.post('/signup', user);
+
+			// we'll come back to here and fill in more when ready
+			request.success(function (data) {
+				// to be filled in on success
+			});
+
+			request.error(function (data) {
+				// to be filled in on error
+			});
+			*/
+		};
+		
+	});
